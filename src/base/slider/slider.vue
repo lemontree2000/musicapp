2c7212a12ac98a3b7da9be1e49d17349f421eb8f
lemontree <gonelove.guo@gmail.com>
2017-07-28 20:07:29 +0800

    slider.vue

---
+++ src/base/slider/slider.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="slider">
+    <div class="slider-group">
+      <slot>
+      </slot>
+    </div>
+  </div>  
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+@import '~common/less/variable';
+.slider {
+  min-height: 1px;
+  .slider-group {
+    position: relative;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+}
+</style>
