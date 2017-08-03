99d403a288021c1e83634dec5fdecce6a565cd18
lemontree <gonelove.guo@gmail.com>
2017-08-03 23:29:30 +0800

    完善 slider

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -4,6 +4,9 @@
       <slot>
       </slot>
     </div>
+    <div class="dots">
+      <span class="dot" v-for="(item, index) in dots" :key="item" :class="{'active': currentPageIndex == index}"></span>
+    </div>
   </div>  
 </template>
 
@@ -11,6 +14,12 @@
 import BScroll from 'better-scroll';
 import { addClass } from 'common/js/dom';
 export default {
+  data() {
+    return {
+      dots: [],
+      currentPageIndex: 0
+    };
+  },
   props: {
     loop: {
       type: Boolean,
@@ -28,24 +37,28 @@ export default {
   mounted () {
     setTimeout(() => {
       this._setSliderWidth();
+      this._initDots();
       this._initSlider();
+      if (this.autoPlay) {
+        this._play();
+      }
     }, 20);
   },
   methods: {
     _setSliderWidth() {
-      this.children = this.$refs.sliderGroup.children;
       let width = 0;
       let sliderWidth = this.$refs.slider.clientWidth;
+      this.children = this.$refs.sliderGroup.children;
       for (let i = 0; i < this.children.length; i++) {
         let child = this.children[i];
         addClass(child, 'slider-item');
         child.style.width = sliderWidth + 'px';
         width += sliderWidth;
-        if (this.loop) {
-          width += 2 * sliderWidth;
-        }
-        this.$refs.sliderGroup.style.width = width + 'px';
       }
+      if (this.loop) {
+        width += 2 * sliderWidth;
+      }
+      this.$refs.sliderGroup.style.width = width + 'px';
     },
     _initSlider() {
       this.slider = new BScroll(this.$refs.slider, {
@@ -58,6 +71,26 @@ export default {
         snapSpeed: 400,
         click: true
       });
+      this.slider.on('scrollEnd', () => {
+        let pageIndex = this.slider.getCurrentPage().pageX;
+        console.log(pageIndex);
+        if (this.loop) {
+          pageIndex -= 1;
+        }
+        this.currentPageIndex = pageIndex;
+      });
+    },
+    _initDots() {
+      this.dots = new Array(this.children.length);
+    },
+    _play() {
+      let pageIndex = this.currentPageIndex + 1;
+      if (this.loop) {
+        pageIndex += 1;
+      }
+      this.timer = setInterval(() => {
+        this.slider.goToPage(pageIndex, 0, 400);
+      }, this.interval);
     }
   }
 };
@@ -85,6 +118,27 @@ export default {
       img {
         display: block;
         width: 100%;
+      }
+    }
+  }
+  .dots{
+    position: absolute;
+    right: 0;
+    left: 0;
+    bottom: 12px;
+    text-align: center;
+    font-size: 0;
+    .dot {
+      display: inline-block;
+      margin: 0 4px;
+      width: 8px;
+      height: 8px;
+      border-radius: 50%;
+      background: @color-text-l;
+      &.active {
+        width: 20px;
+        border-radius: 5px;
+        background: @color-text-ll;
       }
     }
   }
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="recommend">
     <div class="recommend-content">
-      <div class="slider-wrapper" v-if="recommends.length">
+      <div v-if="recommends.length" class="slider-wrapper" >
         <slider>
           <div v-for="(item, index) in recommends" :key="item.id">
             <a :href="item.linkUrl">
-              <img :src="item.picUrl" >
+              <img :src="item.picUrl">
             </a>
           </div>
         </slider>
@@ -43,5 +43,62 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+@import "~common/less/variable";
+  .recommend{
+    position: fixed;
+    width: 100%;
+    top: 88px;
+    bottom: 0;
+    .recommend-content {
+      height: 100%;
+      overflow: hidden;
+      .slider-wrapper {
+        position: relative;
+        width: 100%;
+        // overflow: hidden;
+      }  
+      .recommend-list {
+        .list-title {
+          height: 65px;
+          line-height: 65px;
+          text-align: center;
+          font-size: @font-size-medium;
+          color: @color-theme;
+        }  
+        .item{
+          display: flex;
+          box-sizing: border-box;
+          align-items: center;
+          padding: 0 20px 20px 20px;
+          .icon {
+            flex: 0 0 60px;
+            width: 60px;
+            padding-right: 20px;
+          }
+          .text {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            flex: 1;
+            line-height: 20px;
+            overflow: hidden;
+            font-size: @font-size-medium;
+            .name{
+              margin-bottom: 10px;
+              color: @color-text;
+            }
+            .desc{
+              color: @color-text-d;
+            }
+          }
+        }      
+      }        
+      .loading-container {
+        position: absolute;
+        width: 100%;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+    }    
+  }      
 </style>
