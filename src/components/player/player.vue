e8a2e18a9f003fdc5d3bfbf25e17b82f2a304e4a
lemontree <gonelove.guo@gmail.com>
2017-09-14 23:33:57 +0800

    完成歌词滑动

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -17,8 +17,8 @@
           <h1 class="title" v-html="currentSong.name"></h1>
           <h2 class="subtitle" v-html="currentSong.singer"></h2>
         </div>
-        <div class="middle">
-          <div class="middle-l">
+        <div class="middle" @touchstart.prevent="middleTouchStart" @touchmove.prevent="middleTouchMove" @touchend="middleTouchEnd">
+          <div class="middle-l" ref="middleL">
             <div class="cd-wrapper" ref="cdWraper" >
               <div class="cd" :class="cdClass">
                 <img class="image" :src="currentSong.image">
@@ -41,6 +41,10 @@
           </scroll>
         </div>
         <div class="bottom">
+          <div class="dot-wrapper">
+            <span class="dot" :class="{'active': currentShow === 'cd'}"></span>
+            <span class="dot" :class="{'active': currentShow === 'lyric'}"></span>
+          </div>
           <div class="progress-wrapper">
             <span class="time time-l" v-text="format(currentTime)"></span>
             <div class="progress-bar-wrapper">
@@ -103,6 +107,7 @@ import Lyric from 'lyric-parser';
 import scroll from 'base/scroll/scroll';
 
 const transform = prefixStyle('transform');
+const transitionDuration = prefixStyle('transitionDuration');
 export default {
   data() {
     return {
@@ -110,7 +115,8 @@ export default {
       currentTime: 0,
       radius: 32,
       currentLyric: null,
-      currentLineNum: 0
+      currentLineNum: 0,
+      currentShow: 'cd'
     };
   },
   components: {
@@ -283,6 +289,57 @@ export default {
     error() {
       this.songReady = true;
     },
+    middleTouchMove(e) {
+      if (!this.touch.initiated) {
+        return;
+      }
+      const touch = e.touches[0];
+      let deltaX = touch.pageX - this.touch.startX;
+      let deltaY = touch.pageY - this.touch.startY;
+      if (Math.abs(deltaY) > Math.abs(deltaX)) {
+        return;
+      }
+      const left = this.currentShow === 'cd' ? 0 : -window.innerWidth;
+      const width = Math.min(0, Math.max(-window.innerWidth, left + deltaX));
+      this.touch.percent = Math.abs(width / window.innerWidth);
+      this.$refs.lyriList.$el.style[transform] = `translate3d(${width}px,0,0)`;
+      this.$refs.lyriList.$el.style[transitionDuration] = 0;
+      this.$refs.middleL.style.opacity = 1 - this.touch.percent;
+      this.$refs.middleL.style[transitionDuration] = 0;
+    },
+    middleTouchEnd() {
+      let width;
+      let opacity;
+      if (this.currentShow === 'cd') {
+        if (this.touch.percent > 0.1) {
+          width = -window.innerWidth;
+          this.currentShow = 'lyric';
+          opacity = 0;
+        } else {
+          width = 0;
+          opacity = 1;
+        }
+      } else {
+        if (this.touch.percent < 0.9) {
+          width = 0;
+          this.currentShow = 'cd';
+          opacity = 1;
+        } else {
+          width = -window.innerWidth;
+          opacity = 0;
+        }
+      }
+      this.$refs.lyriList.$el.style[transform] = `translate3d(${width}px,0,0)`;
+      this.$refs.lyriList.$el.style[transitionDuration] = 300 + 'ms';
+      this.$refs.middleL.style.opacity = opacity;
+      this.$refs.middleL.style[transitionDuration] = 300;
+    },
+    middleTouchStart(e) {
+      this.touch.initiated = true;
+      const touch = e.touches[0];
+      this.touch.startX = touch.pageX;
+      this.touch.startY = touch.pageY;
+    },
     format(interval) {
       interval = interval | 0;
       const minute = interval / 60 | 0;
@@ -338,6 +395,9 @@ export default {
         newPlaying ? audio.play() : audio.pause();
       });
     }
+  },
+  created() {
+    this.touch = {};
   }
 };
 </script>
